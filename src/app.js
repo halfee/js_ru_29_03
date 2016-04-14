1e1de13a582dd6cc0f9cdb003a4e20bb28a193cc
half <hobithalf@ya.ru>
2016-04-14 22:02:42 +0500

    merge

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-<<<<<<< HEAD
-import AppContainer from './containers/AppContainer'
-=======
-//import AppContainer from './containers/AppContainer'
-//import { articles } from './fixtures'
 import routes from './routes'
->>>>>>> romabelka/master
 
 render(routes, document.getElementById('container'))
